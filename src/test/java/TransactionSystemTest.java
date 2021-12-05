8ee614e20bac805b387702d2d49f6c583d0faa20
raguramsiva <raguram.sivabalan@outlook.com>
2021-12-05 00:08:46 -0500

    Added Test Cases

---
+++ src/test/java/TransactionSystemTest.java
@@ -0,0 +1,57 @@
+import gateway.DatabaseGateway;
+import org.junit.Test;
+import transactions.TransactionSystem;
+
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionSystemTest {
+
+    @Test
+    public void initializeTransactionTest(){
+
+        ArrayList<String> responses = new ArrayList<>();
+
+        String username = "uncleBob";
+        String password = "solidPrinciples";
+        String product = "Cucumber";
+        String quantity = "10";
+        String mobile = "416-850-8274";
+        String payment = "credit";
+        String address = "555 Main Street";
+        String membership = "Silver Membership";
+
+        responses.add(username);
+        responses.add(password);
+        responses.add(product);
+        responses.add(quantity);
+        responses.add(mobile);
+        responses.add(payment);
+        responses.add(address);
+        responses.add(membership);
+
+        DatabaseGateway gw = new DatabaseGateway();
+        TransactionSystem ts = new TransactionSystem(gw);
+
+        ArrayList<String> expected = new ArrayList<>();
+
+        expected.add("");
+        expected.add("Transaction Summary:");
+        expected.add("You have added 10 units of Cucumber priced at $0.85 each.");
+        expected.add("You have added 1 units of Silver Membership priced at $50.00 each.");
+        expected.add("Your total after membership discount is $58.50.");
+
+        ArrayList<String> actual = ts.initializeTransaction(responses, "Customer");
+
+        assertEquals(expected, actual);
+
+    }
+
+
+
+
+}
